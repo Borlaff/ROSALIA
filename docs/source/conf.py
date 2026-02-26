2a358d0c1397c1bad25a514f5a9d9f4552933cf5
Borlaff <asborlaff@gmail.com>
2026-02-26 10:29:09 -0800

    Removing dark mode sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,7 @@ extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary',
-    "sphinx-rtd-dark-mode"
-]
+    'sphinx.ext.autosummary']
 
 autosummary_generate = True  # Turn on sphinx.ext.autosummary
 templates_path = ['_templates']
