85a950c0f3eebcf4094f0f4835ea546e84c0a39a
Pablo M Sanchez Alarcon <pmsa.astro@gmail.com>
2026-09-16 13:59:17 -0700

    better pylance understanding

--- rosalia/__init__.py
+++ rosalia/__init__.py
@@ -15,34 +15,64 @@ try:
 except PackageNotFoundError:
    __version__ = "1.2.4.dev0"
 
-__name__ = "ROSALIA"
+
 __author__ = "Alejandro S. Borlaff"
 __author_email__ = "a.s.borlaff@nasa.gov"
 __description__ = "A software to calibrate the sky background of Space Telescope images"
 __url__ = "https://github.com/Borlaff/ROSALIA"
 
-import rosalia
-import rosalia.utils
-import rosalia.astrometry
-import rosalia.bootima
-import rosalia.correct
-import rosalia.core
-import rosalia.detectors
-import rosalia.horizons
-import rosalia.irsa
-import rosalia.ndi
-import rosalia.psf
-import rosalia.gaia
-import rosalia.gnu
-import rosalia.roman
-import rosalia.sky
-import rosalia.skysurf
-import rosalia.sso
-import rosalia.skywalker
-import rosalia.telescopes
-import rosalia.tests # No need to run the tests every time we load the module.
-import rosalia.constants
-import rosalia.plots
-import rosalia.albedo
-import rosalia.render
-import rosalia.attitude
+from . import (
+    albedo as albedo,
+    astrometry as astrometry,
+    attitude as attitude,
+    bootima as bootima,
+    constants as constants,
+    core as core,
+    correct as correct,
+    detectors as detectors,
+    gaia as gaia,
+    gnu as gnu,
+    horizons as horizons,
+    hst as hst,
+    irsa as irsa,
+    ndi as ndi,
+    plots as plots,
+    psf as psf,
+    render as render,
+    roman as roman,
+    sky as sky,
+    skysurf as skysurf,
+    skywalker as skywalker,
+    sso as sso,
+    telescopes as telescopes,
+    tests as tests,
+    utils as utils,
+)
+
+__all__ = [
+    "utils",
+    "astrometry",
+    "bootima",
+    "correct",
+    "core",
+    "detectors",
+    "horizons",
+    "irsa",
+    "ndi",
+    "psf",
+    "gaia",
+    "gnu",
+    "roman",
+    "sky",
+    "skysurf",
+    "sso",
+    "skywalker",
+    "telescopes",
+    "tests",
+    "constants",
+    "plots",
+    "albedo",
+    "render",
+    "attitude",
+    "roman_estimate_straylight_SCA"
+]
