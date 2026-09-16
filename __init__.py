a73ba4f0eff25b1360f8692e47d0f22f8139b68d
Pablo M Sanchez Alarcon <pmsa.astro@gmail.com>
2026-09-16 13:18:00 -0700

    converge inits files, cleaning

--- __init__.py
+++
@@ -1,7 +0,0 @@
-"""
-ROSALIA: ROman Sky Analyst for Low surface brightness Imaging & Astronomy
-
-Alejandro S. Borlaff
-NASA Ames Research Center, Moffett Field, 94035, California, USA.
-a.s.borlaff@nasa.gov
-"""
--- rosalia/__init__.py
+++ rosalia/__init__.py
@@ -1,6 +1,9 @@
 """
 ROSALIA: ROman Sky Analyst for Low surface brightness Imaging & Astronomy
 
+Alejandro S. Borlaff
+NASA Ames Research Center, Moffett Field, 94035, California, USA.
+a.s.borlaff@nasa.gov
 """
 
 __name__ = "ROSALIA"
